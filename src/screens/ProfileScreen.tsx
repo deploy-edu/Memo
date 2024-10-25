dd6829f8955600051da342f02383492f660e89bc
Juwon Jeong <2jw0718@gmail.com>
2024-10-25 14:50:33 +0900

    회원정보 페이지 변경

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,17 +1,76 @@
 import styled from "@emotion/native";
-import React, { FC } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import React, { FC, useState } from "react";
 import CommonText from "../components/CommonText";
+import Input from "../components/Input";
+import { useAuthStore } from "../stores/useAuthStore";
 
 const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  padding: 20px;
+  gap: 20px;
+`;
+
+const Title = styled(CommonText)`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Value = styled(CommonText)`
+  font-size: 20px;
+`;
+
+const ItemContainer = styled.View`
+  justify-content: space-between;
+  gap: 10px;
+`;
+
+const StyledInput = styled(Input)`
+  font-size: 20px;
+  border-width: 1px;
 `;
 
 const ProfileScreen: FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [avatarUrl, setAvatarUrl] = useState<string>("");
+  const [birth, setBirth] = useState<Date | null>();
+  const [username, setUsername] = useState<string>("");
+  const session = useAuthStore((state) => state.session);
+
   return (
     <Container>
-      <CommonText>ProfileScreen</CommonText>
+      <ItemContainer>
+        <Title>email</Title>
+        <Value>{email}</Value>
+      </ItemContainer>
+      <ItemContainer>
+        <Title>이름</Title>
+        <StyledInput
+          placeholder="이름을 입력하세요.."
+          value={username}
+          onChangeText={(text) => {
+            setUsername(text);
+          }}
+        />
+      </ItemContainer>
+      <ItemContainer>
+        <Title>생년월일</Title>
+        <DateTimePicker
+          style={{
+            borderWidth: 1,
+            borderRadius: 5,
+          }}
+          display="spinner"
+          mode="date"
+          value={birth || new Date()}
+          onChange={(event, date) => {
+            if (event.type === "set") {
+              setBirth(date);
+            }
+          }}
+        />
+      </ItemContainer>
     </Container>
   );
 };
