0807ac4cf47b72552ba4ec14f5f7622a72f48c33
Juwon Jeong <2jw0718@gmail.com>
2024-10-25 10:57:40 +0900

    로그아웃 로직 연동

--- components/screens/SettingScreen.tsx
+++ components/screens/SettingScreen.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/native";
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
 import SettingListItem from "../components/SettingListItem";
+import { useAuthStore } from "../stores/useAuthStore";
 
 const Container = styled.View`
   flex: 1;
@@ -10,9 +11,13 @@ const Container = styled.View`
 `;
 
 const SettingScreen: FC = () => {
+  const onLogout = useCallback(() => {
+    useAuthStore.getState().logout();
+  }, []);
+
   return (
     <Container>
-      <SettingListItem title="로그아웃" />
+      <SettingListItem title="로그아웃" onPress={onLogout} />
     </Container>
   );
 };
