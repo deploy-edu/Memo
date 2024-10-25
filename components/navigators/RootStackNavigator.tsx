e9518c3f5f9d66791db481d5db5e3a9e20803532
Juwon Jeong <2jw0718@gmail.com>
2024-10-25 10:37:30 +0900

    로그인 여부 체크 상태 추가

--- components/navigators/RootStackNavigator.tsx
+++ components/navigators/RootStackNavigator.tsx
@@ -1,20 +1,32 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { useState } from "react";
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
 import MainTabNavigator from "./MainTabNavigator";
 
 type RootStackParamList = {
   MainTab: undefined;
+  Login: undefined;
+  SignUp: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="MainTab" component={MainTabNavigator} />
+      {isLoggedIn ? (
+        <Stack.Screen name="MainTab" component={MainTabNavigator} />
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
   );
 }
