4b59160d8381e5f6f1f626119a22c9c7ecdcae21
Juwon Jeong <2jw0718@gmail.com>
2024-10-25 15:41:23 +0900

    MemoListItem 추가

---
+++ src/components/MemoListItem.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/native";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import React, { FC } from "react";
+import { ViewStyle } from "react-native";
+import CommonText from "./CommonText";
+
+type Props = {
+  style?: ViewStyle;
+  title?: string;
+  onPress?: () => void;
+};
+
+const Container = styled.Pressable`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled(CommonText)`
+  font-size: 15px;
+`;
+
+const MemoListItem: FC<Props> = ({ style, title, onPress }) => {
+  return (
+    <Container style={style} onPress={onPress}>
+      <Title>{title}</Title>
+      <Icon name="chevron-right" size={20} />
+    </Container>
+  );
+};
+
+export default MemoListItem;
