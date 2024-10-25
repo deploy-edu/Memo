66acaa90a059ee727080f5c42d812135bd642244
Juwon Jeong <2jw0718@gmail.com>
2024-10-25 14:18:15 +0900

    인증 상태 변경 이벤트 등록 및 앱시작시 인증 정보 체크 로직 추가

--- src/navigators/RootStackNavigator.tsx
+++ src/navigators/RootStackNavigator.tsx
@@ -1,4 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { useEffect } from "react";
+import { AppState } from "react-native";
+import { supabase } from "../libs/supabase";
 import LoginScreen from "../screens/LoginScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import { useAuthStore } from "../stores/useAuthStore";
@@ -10,10 +13,38 @@ export type RootStackParamList = {
   SignUp: undefined;
 };
 
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
+
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (!session) {
+        useAuthStore.getState().logout();
+        return;
+      }
+
+      useAuthStore.getState().login(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (!session) {
+        useAuthStore.getState().logout();
+        return;
+      }
+
+      useAuthStore.getState().login(session);
+    });
+  }, []);
 
   return (
     <Stack.Navigator
